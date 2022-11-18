c2b300d877933921f0c304f8c8072605d1755d46
Arif00 <arif.cihangir@yh.nackademin.se>
2022-11-18 13:25:53 +0100

    adding car.py

---
+++ shop_app/car.py
@@ -0,0 +1,20 @@
+'''Info'''
+class Car:
+    '''Info dedim'''
+    def __init__(self, color, doors, power):
+        self.color = color
+        self.doors = doors
+        self.power = power
+
+
+    def set_color(self, color):
+        '''Info'''
+        self.color = color
+
+    def set_doors(self,doors):
+        '''Info'''
+        self.doors = doors
+
+    def set_power(self, power):
+        '''Info'''
+        self.power = power
